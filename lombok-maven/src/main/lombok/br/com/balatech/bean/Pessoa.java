e3021156230c256e1568fc4500d06f00e98f4adf
Bala-Tech <balatecnologia@gmail.com>
2019-06-02 16:36:04 -0300

    add maven project

---
+++ lombok-maven/src/main/lombok/br/com/balatech/bean/Pessoa.java
@@ -0,0 +1,22 @@
+package br.com.balatech.bean;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.java.Log;
+
+@Data
+@Builder
+@Log
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    public String imprimirNome() {
+
+        log.info("Imprimindo nome....");
+
+        return nome;
+    }
+}
